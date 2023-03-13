82025050470621a6bf505a3c9f9a3bf750692183
Max Yoo <devbear74@gmail.com>
2023-03-13 16:58:30 +0900

    Add App Widget Example & Resources

---
+++ app/src/main/java/com/gandalgom/sample/navigation/DeepLinkAppWidget.kt
@@ -0,0 +1,45 @@
+package com.gandalgom.sample.navigation
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.os.Bundle
+import android.widget.RemoteViews
+
+/**
+ * Implementation of App Widget functionality.
+ */
+class DeepLinkAppWidget : AppWidgetProvider() {
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        // There may be multiple widgets active, so update all of them
+        for (appWidgetId in appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId)
+        }
+    }
+
+    override fun onEnabled(context: Context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    override fun onDisabled(context: Context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
+
+internal fun updateAppWidget(
+    context: Context,
+    appWidgetManager: AppWidgetManager,
+    appWidgetId: Int
+) {
+    // Construct the RemoteViews object
+    val remoteViews = RemoteViews(context.packageName, R.layout.deep_link_app_widget)
+    val args = Bundle()
+    args.getString("MyArgument", "From Widget")
+
+    // Instruct the widget manager to update the widget
+    appWidgetManager.updateAppWidget(appWidgetId, remoteViews)
+}
