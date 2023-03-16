5a370afb6ff13a8667a60b5fcb99f704ee1d49d3
Max Yoo <devbear74@gmail.com>
2023-03-16 17:43:19 +0900

    Navigating using menus, drawers and bottom navigation

--- app/src/main/java/com/gandalgom/sample/navigation/MainActivity.kt
+++ app/src/main/java/com/gandalgom/sample/navigation/MainActivity.kt
@@ -4,10 +4,21 @@ import android.content.res.Resources
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
 
@@ -27,7 +38,17 @@ class MainActivity : AppCompatActivity() {
         // Set up Action Bar
         val navController = host.navController
 
-        appBarConfiguration = AppBarConfiguration(navController.graph)
+        val drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
+        appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.home_screen, R.id.deep_link_screen),
+            drawerLayout,
+        )
+
+        setupActionBar(navController, appBarConfiguration)
+
+        setupBottomNavigationMenu(navController)
+
+        setupNavigationMenu(navController)
 
         navController.addOnDestinationChangedListener { _, destination, _ ->
             val dest: String = try {
@@ -43,5 +64,40 @@ class MainActivity : AppCompatActivity() {
             ).show()
             Log.d("MainActivity", "Navigated to $dest")
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val navigationView = findViewById<NavigationView>(R.id.navigation_view)
+        if (navigationView == null) {
+            menuInflater.inflate(R.menu.overflow_menu, menu)
+            return true
+        }
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return item.onNavDestinationSelected(findNavController(R.id.my_nav_host_fragment))
+                || super.onOptionsItemSelected(item)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.my_nav_host_fragment).navigateUp(appBarConfiguration)
+    }
+
+    private fun setupActionBar(
+        navController: NavController,
+        appBarConfiguration: AppBarConfiguration,
+    ) {
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    private fun setupBottomNavigationMenu(navController: NavController) {
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation_view)
+        bottomNavigationView?.setupWithNavController(navController)
+    }
+
+    private fun setupNavigationMenu(navController: NavController) {
+        val navigationView = findViewById<NavigationView>(R.id.navigation_view)
+        navigationView?.setupWithNavController(navController)
     }
 }
