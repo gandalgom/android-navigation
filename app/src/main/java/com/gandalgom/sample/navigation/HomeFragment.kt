ba7bd406c32c1d498df33fa45b343adf38281fc9
Max Yoo <devbear74@gmail.com>
2023-03-16 15:10:42 +0900

    Add UI Screens of MainActivity for supporting multi screen

---
+++ app/src/main/java/com/gandalgom/sample/navigation/HomeFragment.kt
@@ -0,0 +1,41 @@
+package com.gandalgom.sample.navigation
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import androidx.core.view.MenuProvider
+import androidx.lifecycle.Lifecycle
+
+/**
+ * Fragment used to show how to navigate to another destination
+ */
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Add menu items without using the Fragment Menu APIs
+        // Note how we can tie the MenuProvider to the viewLifecycleOwner
+        // and an optional Lifecycle.State (here, RESUMED) to indicate when
+        // the menu should be visible
+        requireActivity().addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                // Add menu items here
+                menuInflater.inflate(R.menu.main_menu, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                // TODO: Handle the menu selection
+                return false
+            }
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
+    }
+}
